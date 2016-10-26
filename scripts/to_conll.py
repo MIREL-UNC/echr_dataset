4d22b95659f0d43422b88d3d8a8da68920d400f2
Milagro Teruel <milagro.teruel@gmail.com>
2016-10-26 15:18:43 -0300

    Addings more information to conll format

--- scripts/to_conll.py
+++ scripts/to_conll.py
@@ -1,7 +1,9 @@
 """Script to pass to CoNLL format a document downloaded using the crawler.
 
 The output format is
-word_token \t POS tag
+    index \t word_token \t POS tag \t label
+The index is the position of the word_token in the sentence. The label is 'O'
+for words in the document, and 'O-DOC' for the document title.
 
 Sentences are separated by blank lines.
 """
@@ -16,21 +18,30 @@ import tqdm
 def parse_arguments():
     """Returns the stdin arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_filepath', type=unicode)
-    parser.add_argument('-o', '--output_filepath', type=unicode)
+    parser.add_argument('-i', '--input-filepath', type=unicode)
+    parser.add_argument('-o', '--output-filepath', type=unicode)
 
     return parser.parse_args()
 
 
+def write_sentence(sentence, output_file, label):
+    """Tokenizes the sentence and saves it with the given label."""
+    words = nltk.tokenize.word_tokenize(sentence)
+    for index, (word, tag) in enumerate(nltk.pos_tag(words)):
+        line = u'{}\t{}\t{}\t{}\n'.format(index, word, tag, label)
+        output_file.write(line.encode('utf-8'))
+    output_file.write(u'\n')
+
+
 def write_document(document, output_file):
     """Tokenizes and saves the document."""
+    # Write the title
+    write_sentence(document['title'], output_file, 'O-DOC')
+
+    # Write the content
     for paragraph in document['sentences']:
         for sentence in nltk.tokenize.sent_tokenize(paragraph):
-            words = nltk.tokenize.word_tokenize(sentence)
-            for word, tag in nltk.pos_tag(words):
-                output_file.write(
-                    (u'{}\t{}\n'.format(word, tag)).encode('utf-8'))
-            output_file.write(u'\n')
+            write_sentence(sentence, output_file, 'O')
 
 
 def get_document_number(filename):
