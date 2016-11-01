99305c41bc9858093231bf10e5d77f3f27db04f9
Milagro Teruel <milagro.teruel@gmail.com>
2016-11-01 18:18:05 -0300

    Simple BoW representation

--- scripts/distance_measure/preprocess.py
+++ scripts/distance_measure/preprocess.py
@@ -6,6 +6,7 @@ import logging
 logging.basicConfig(level=logging.INFO, filename='.log-preprocess')
 import numpy
 import os
+import pickle
 import re
 import subprocess
 import tqdm
@@ -14,17 +15,29 @@ import tqdm
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import SGDClassifier
+from sklearn.manifold import TSNE
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.utils import shuffle
 
 
+MAX_TSNE_SAMPLE = 1000
+
+
 def parse_arguments():
     """Returns the stdin arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('--echr-dirpath', type=unicode)
-    parser.add_argument('--wiki-filepath', type=unicode)
+    parser.add_argument('--echr-dirpath', type=unicode,
+                        help='Directory with the jl (replaced) files. Documents'
+                        ' must be separated by a blank line.')
+    parser.add_argument('--wiki-filepath', type=unicode,
+                        help='File with the wikipedia documents. Each document'
+                        ' is in a line of the file.')
+    parser.add_argument('--output_filename', type=unicode,
+                        help='File path to save the output')
+    parser.add_argument('--save_tsne', action="store_true",
+                        help='Save two dimension tsne vectors.')
 
     return parser.parse_args()
 
@@ -45,7 +58,7 @@ def get_input_files(input_dirpath, pattern):
 
 def read_echr_documents(input_dirpath):
     """Returns a list of documents"""
-    filepaths = get_input_files(input_dirpath, r'.*jl')
+    filepaths = get_input_files(input_dirpath, r'.*jl.sample')
     result = []
     for filepath in filepaths:
         total_docs = get_document_number(filepath)
@@ -59,7 +72,7 @@ def read_wiki_documents(input_filepath):
     """Returns a list with the wikipedia documents."""
     total_docs = get_document_number(input_filepath)
     with open(input_filepath, 'r') as input_file:
-        result = [line for line in tqdm.tqdm(
+        result = [line.decode('utf-8') for line in tqdm.tqdm(
             input_file.readlines(), total=total_docs)]
     return result
 
@@ -75,44 +88,58 @@ def log_report(predictions, y_test):
         logging.info('\t'.join([str(count) for count in row]))
 
 
-
-def evaluate(pipeline, x_matrix, y_vector):
-    """Trains and applies the pipeline to x_matrix, y_vector."""
+def evaluate(classifier, x_matrix, y_vector):
+    """Trains and applies the classifier to x_matrix, y_vector."""
     logging.info('Splitting dataset')
     x_train, x_test, y_train, y_test = train_test_split(
         x_matrix, y_vector, test_size=0.33)
 
     logging.info('Training classifier')
-    pipeline.fit(x_train, y_train)
+    classifier.fit(x_train, y_train)
 
     logging.info('Getting predictions')
-    predictions = pipeline.predict(x_test)
+    predictions = classifier.predict(x_test)
     log_report(predictions, y_test)
 
 
 def main():
     """Main function of script."""
     args = parse_arguments()
+    numpy.set_printoptions(suppress=True)
 
     logging.info('Reading ECHR documents')
     echr_documents = read_echr_documents(args.echr_dirpath)  # ~1GB
     logging.info('Reading Wikipedia documents')
     wiki_documents = read_wiki_documents(args.wiki_filepath)  # ~1GB
-
-    x_matrix = numpy.array(echr_documents + wiki_documents)
+    x_matrix = numpy.array(echr_documents + wiki_documents, dtype=unicode)
     y_vector = numpy.array(
         [0] * len(echr_documents) + [1] * len(wiki_documents))
 
-    x_matrix, y_vector = numpy.shuffle(x_matrix, y_vector)
+    x_matrix, y_vector = shuffle(x_matrix, y_vector)
 
-    pipeline = Pipeline([
-        ('word_count', CountVectorizer(max_features=10**5)),
-        ('tfidf', TfidfTransformer()),
-        ('classifier', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-2,
-                                  n_iter=5, random_state=42, n_jobs=-1))
-    ])
+    features = [('word_count', CountVectorizer(max_features=10**5)),
+                ('tfidf', TfidfTransformer())]
+    feature_pipeline = Pipeline(features)
+    transformed_matrix = feature_pipeline.fit_transform(x_matrix)
+    del x_matrix
 
+    classifier = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-2,
+                               n_iter=5, random_state=42, n_jobs=-1)
 
+    evaluate(classifier, transformed_matrix, y_vector)
+
+    if args.save_tsne:
+        tsne_model = TSNE(n_components=2)
+        if transformed_matrix.shape[0] > MAX_TSNE_SAMPLE:
+            vectors = transformed_matrix[:MAX_TSNE_SAMPLE].toarray()
+            labels = y_vector[:MAX_TSNE_SAMPLE]
+        else:
+            vectors = transformed_matrix.toarray()
+            labels = y_vector
+        vectors = tsne_model.fit_transform(vectors)
+        with open(args.output_filename, 'w') as output_file:
+            output = {'vectors': vectors, 'labels': labels}
+            pickle.dump(output, output_file)
 
 
 if __name__ == '__main__':
