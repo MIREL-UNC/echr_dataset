4fc7ba829e016172c9a39887deb5a1e97ff389d9
Milagro Teruel <milagro.teruel@gmail.com>
2016-11-16 07:19:03 -0300

    To conll script reads all files in directory

--- scripts/to_conll.py
+++ scripts/to_conll.py
@@ -1,4 +1,4 @@
-"""Script to pass to CoNLL format a document downloaded using the crawler.
+"""Script to pass to CoNLL format documents downloaded using the crawler.
 
 The output format is
     index \t word_token \t POS tag \t label
@@ -11,6 +11,8 @@ Sentences are separated by blank lines.
 import argparse
 import jsonlines
 import nltk
+import os
+import re
 import subprocess
 import tqdm
 
@@ -18,8 +20,8 @@ import tqdm
 def parse_arguments():
     """Returns the stdin arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input-filepath', type=unicode)
-    parser.add_argument('-o', '--output-filepath', type=unicode)
+    parser.add_argument('-i', '--input-dirpath', type=unicode)
+    parser.add_argument('-o', '--output-dirpath', type=unicode)
 
     return parser.parse_args()
 
@@ -49,16 +51,27 @@ def get_document_number(filename):
     return int(subprocess.check_output(["wc", "-l", filename]).split()[0])
 
 
+def get_input_files(input_dirpath, pattern):
+    """Returns the names of the files in input_dirpath that matches pattern."""
+    all_files = os.listdir(input_dirpath)
+    for filename in all_files:
+        if re.match(pattern, filename) and os.path.isfile(os.path.join(
+                input_dirpath, filename)):
+            yield os.path.join(input_dirpath, filename)
+
+
 def main():
     """Main script function"""
     args = parse_arguments()
-
-    total_docs = get_document_number(args.input_filepath)
-
-    with open(args.output_filepath, 'w') as output_file:
-        with jsonlines.open(args.input_filepath) as reader:
-            for document in tqdm.tqdm(reader, total=total_docs):
-                write_document(document, output_file)
+    filepaths = get_input_files(args.input_dirpath, r'.*jl')
+    for filepath in filepaths:
+        total_docs = get_document_number(filepath)
+        filename = '{}.conll'.format(os.path.basename(filepath).split('.jl')[0].encode('utf-8'))
+        output_filename = os.path.join(args.output_dirpath, filename.decode('utf8'))
+        with open(output_filename, 'w') as output_file:
+            with jsonlines.open(filepath) as reader:
+                for document in tqdm.tqdm(reader, total=total_docs):
+                    write_document(document, output_file)
 
 
 if __name__ == '__main__':
